0141e48a29da14f855c5f0bd21c1feac3cb8b25f
Andriansyah <andriansyahgl@gmail.com>
2016-02-22 21:02:06 +0700

    update

--- ABSYS.java
+++ ABSYS.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author AndoriGL
- */
 import java.util.Scanner;
 public class ABSYS {
     public static void main(String[] args) {
